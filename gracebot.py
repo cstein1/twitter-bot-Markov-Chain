c1031305d2a0b1cbcb0f3b0847e09243ca725fcf
cstein1 <cstein1@trinity.edu>
2018-12-20 17:32:50 -0500

    Working base model

---
+++ gracebot.py
@@ -0,0 +1,67 @@
+
+import random
+import sys
+
+def generateSentence(uname, seedword = "my"):
+    with open(uname + "_tweets.txt", "r", encoding='utf-8') as file:
+        lines = file.read().lower()
+    # Split by <EOS> end of string
+    payload = lines.split("<eos>")
+    # Get rid of <SOS> start of string
+    payload = [txt[5:] for txt in payload]
+    grdic = makedic(payload)
+    for _ in range(10):
+        makeSentence(grdic,seedword,10)
+        print()
+
+def makedic(payload):
+    grdic = {}
+    for line in payload:
+        for word,nxtword in window(line.split(),2):
+            if word in grdic:
+                if nxtword in grdic[word]:
+                    grdic[word][nxtword]+=1
+                else:
+                    grdic[word][nxtword]=1
+            else:
+                grdic[word] = {nxtword:1}
+    return grdic
+
+def makeSentence(grdic, startseed, notimes):
+    # Makes full sentence
+    sys.stdout.write(startseed)
+    newseedword = takeLastWordAndPredict(grdic,startseed)
+    for _ in range(notimes):
+        newseedword = takeLastWordAndPredict(grdic,newseedword)
+        if newseedword == "<WNF>":
+            sys.stdout.write(".")
+            break;
+        else:
+            sys.stdout.write(" ")
+            try:sys.stdout.write(newseedword)
+            except:sys.stdout.write("*emoji*")
+    else: #If `break;` isn't hit
+        sys.stdout.write(".")
+
+def takeLastWordAndPredict(dic,word):
+    #Finds next word
+    if word.lower() not in dic:
+        return "<WNF>"
+    probdic = []
+    for i in list(dic[word].items()):
+        for _ in range(i[1]):
+            probdic.append(i[0])
+    nxtwordind = random.randint(0,len(probdic)-1)
+    return probdic[nxtwordind]
+
+def window(iterator, window_width):
+    inds = [[a for a in range(i-window_width,i)] for i in range(window_width, len(iterator)+1)]
+    return [tuple([iterator[i] for i in ind]) for ind in inds]
+
+def printNestedDic(dic):
+    for key, indic in dic.items():
+        try:print(key)
+        except:print("*emoji*")
+        for inkey, count in indic.items():
+            try:print("\t" + inkey + ": " + str(count))
+            except:print("\t*emoji*: " + str(count))
---
+++ tweetdump.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import tweepy #https://github.com/tweepy/tweepy
+import csv
+import re
+
+
+def get_all_tweets(screen_name, consumer_key, consumer_secret, access_key, access_secret):
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []
+
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        print("getting tweets before %s" % (oldest))
+
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+
+        print( "...%s tweets downloaded so far" % (len(alltweets)))
+
+        #outtweets = [tweet.text.encode("utf-8") for tweet in alltweets]
+
+    # longestString = 0
+    f = open("%s_tweets.txt" % screen_name, "wb")
+    for (ind,i) in enumerate(alltweets):
+        tmp = ""
+        try:
+            if not i.text.startswith("RT",0,2):
+                tmp += "<SOS>"
+                tmp += re.sub(r"http\S+", "", str(i.text))
+                #if(len(tmp) > longestString): longestString = len(tmp)
+                tmp += "<EOS>"
+        except:
+            print("Encountered tweet with issue")
+            pass
+        tmp = tmp.encode("utf-8")
+        f.write(tmp)
+    f.close()
